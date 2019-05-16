514847183b22c8003afb119db8fff2a7cd2b18bd
gcem <34423323+gcem@users.noreply.github.com>
2019-05-16 15:17:50 +0300

    Exercise 3.1 (broken)

--- exc301-hemisphere.cpp
+++ exc301-hemisphere.cpp
@@ -2,6 +2,8 @@
 // hemisphere.cpp
 //
 // This program approximates a hemisphere with an array of latitudinal triangle strips.
+// It stores the full list of vertices in an array to avoid calculating them on each call
+// to drawScene
 //
 // Interaction:
 // Press P/p to increase/decrease the number of longitudinal slices.
@@ -34,156 +36,189 @@ static float R = 5.0; // Radius of hemisphere.
 static int p = 6; // Number of longitudinal slices.
 static int q = 4; // Number of latitudinal slices.
 static float Xangle = 0.0, Yangle = 0.0, Zangle = 0.0; // Angles to rotate hemisphere.
+static float *vertices = NULL;
+static unsigned int *indices = NULL;
+
+// Calculates the vertex coordinates and stores them in vertices
+// Creates the indices array
+void updateVertexArray()
+{
+  int i, j;
+  
+  // allocate vertex array
+  free(vertices);
+  if ((vertices = (float*) malloc(3 * p * (q + 1) * sizeof(float))) == NULL) exit(1);
+
+  glVertexPointer(3, GL_FLOAT, 0, vertices);
+  
+  // allocate index array
+  free(indices);
+  if ((indices = (unsigned int*) malloc(2 * (p + 2) * q * sizeof(unsigned int))) == NULL) exit(1);
+
+  // find vertex coordinates and create the index list, line by line
+  for (i = 0; i <= q; i++)
+    {
+      for (j = 0; j < p; j++)
+	{	  
+	  vertices[p * i * 3 + j * 3] = R * cos(PI / 2 * i / q)  * cos(2 * PI * j / p); // x
+	  vertices[p * i * 3 + j * 3 + 1] = R * sin(PI / 2 * i / q); // y
+	  vertices[p * i * 3 + j * 3 + 2] = R * cos(PI / 2 * i / q)  * sin(2 * PI * j / p); // z
+
+	  indices[i * (p + 1) + 2 * j] = i * p + j;
+	  indices[i * (p + 1) + 2 * j + 1] = (i + 1) * p + j;
+	}
+      // repeat the first two vertices
+      indices[i * (p + 1) + 2 * p] = i * p;
+      indices[i * (p + 1) + 2 * p + 1] = (i + 1) * p;
+    }
+}
 
 // Initialization routine.
 void setup(void) 
 {
-   glClearColor(1.0, 1.0, 1.0, 0.0); 
+  glEnableClientState(GL_VERTEX_ARRAY);
+    
+  updateVertexArray();
+  glClearColor(1.0, 1.0, 1.0, 0.0); 
 }
 
 // Drawing routine.
 void drawScene(void)
 {
-   int  i, j;
+  int  i, j;
 
-   glClear (GL_COLOR_BUFFER_BIT);
+  glClear (GL_COLOR_BUFFER_BIT);
 
-   glLoadIdentity();
+  glLoadIdentity();
 
-   // Command to push the hemisphere, which is drawn centered at the origin, 
-   // into the viewing frustum.
-   glTranslatef(0.0, 0.0, -10.0);
+  // Command to push the hemisphere, which is drawn centered at the origin, 
+  // into the viewing frustum.
+  glTranslatef(0.0, 0.0, -10.0);
 
-   // Commands to turn the hemisphere.
-   glRotatef(Zangle, 0.0, 0.0, 1.0);
-   glRotatef(Yangle, 0.0, 1.0, 0.0);
-   glRotatef(Xangle, 1.0, 0.0, 0.0);
+  // Commands to turn the hemisphere.
+  glRotatef(Zangle, 0.0, 0.0, 1.0);
+  glRotatef(Yangle, 0.0, 1.0, 0.0);
+  glRotatef(Xangle, 1.0, 0.0, 0.0);
    
-   // Hemisphere properties.
-   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-   glColor3f(0.0, 0.0, 0.0);
+  // Hemisphere properties.
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glColor3f(0.0, 0.0, 0.0);
 
-   // Array of latitudinal triangle strips, each parallel to the equator, stacked one
-   // above the other from the equator to the north pole.
-   for(j = 0; j < q; j++)
-   {
-      // One latitudinal triangle strip.
-      glBegin(GL_TRIANGLE_STRIP);
-         for(i = 0; i <= p; i++)
-		 {
-            glVertex3f( R * cos( (float)(j+1)/q * PI/2.0 ) * cos( 2.0 * (float)i/p * PI ),
-                        R * sin( (float)(j+1)/q * PI/2.0 ),
-					    R * cos( (float)(j+1)/q * PI/2.0 ) * sin( 2.0 * (float)i/p * PI ) );
-            glVertex3f( R * cos( (float)j/q * PI/2.0 ) * cos( 2.0 * (float)i/p * PI ),
-                        R * sin( (float)j/q * PI/2.0 ),
-					    R * cos( (float)j/q * PI/2.0 ) * sin( 2.0 * (float)i/p * PI ) );         
-		 }
-      glEnd();
-   }
+  // Array of latitudinal triangle strips, each parallel to the equator, stacked one
+  // above the other from the equator to the north pole.
+  for (i = 0; i < q; i++)
+    {
+      // draw the strip
+      glDrawElements(GL_TRIANGLE_STRIP, 2 * (p + 1), GL_UNSIGNED_INT, indices + i * (p + 1));
+    }
 
-   glFlush();
+  glFlush();
 }
 
 // OpenGL window reshape routine.
 void resize(int w, int h)
 {
-   glViewport(0, 0, w, h); 
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
-   glMatrixMode(GL_MODELVIEW);
+  glViewport(0, 0, w, h); 
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 100.0);
+  glMatrixMode(GL_MODELVIEW);
 }
 
 // Keyboard input processing routine.
 void keyInput(unsigned char key, int x, int y)
 {
-   switch(key) 
-   {
-      case 27:
-         exit(0);
-         break;
-      case 'P':
-         p += 1;
-         glutPostRedisplay();
-         break;
-      case 'p':
-         if (p > 3) p -= 1;
-         glutPostRedisplay();
-         break;
-      case 'Q':
-         q += 1;
-         glutPostRedisplay();
-         break;
-      case 'q':
-         if (q > 3) q -= 1;
-         glutPostRedisplay();
-         break;
-      case 'x':
-         Xangle += 5.0;
-		 if (Xangle > 360.0) Xangle -= 360.0;
-         glutPostRedisplay();
-         break;
-      case 'X':
-         Xangle -= 5.0;
-		 if (Xangle < 0.0) Xangle += 360.0;
-         glutPostRedisplay();
-         break;
-      case 'y':
-         Yangle += 5.0;
-		 if (Yangle > 360.0) Yangle -= 360.0;
-         glutPostRedisplay();
-         break;
-      case 'Y':
-         Yangle -= 5.0;
-		 if (Yangle < 0.0) Yangle += 360.0;
-         glutPostRedisplay();
-         break;
-      case 'z':
-         Zangle += 5.0;
-		 if (Zangle > 360.0) Zangle -= 360.0;
-         glutPostRedisplay();
-         break;
-      case 'Z':
-         Zangle -= 5.0;
-		 if (Zangle < 0.0) Zangle += 360.0;
-         glutPostRedisplay();
-         break;
-      default:
-         break;
-   }
+  switch(key) 
+    {
+    case 27:
+      exit(0);
+      break;
+    case 'P':
+      p += 1;
+      updateVertexArray();
+      glutPostRedisplay();
+      break;
+    case 'p':
+      if (p > 3) p -= 1;
+      updateVertexArray();
+      glutPostRedisplay();
+      break;
+    case 'Q':
+      q += 1;
+      updateVertexArray();
+      glutPostRedisplay();
+      break;
+    case 'q':
+      if (q > 3) q -= 1;
+      updateVertexArray();
+      glutPostRedisplay();
+      break;
+    case 'x':
+      Xangle += 5.0;
+      if (Xangle > 360.0) Xangle -= 360.0;
+      glutPostRedisplay();
+      break;
+    case 'X':
+      Xangle -= 5.0;
+      if (Xangle < 0.0) Xangle += 360.0;
+      glutPostRedisplay();
+      break;
+    case 'y':
+      Yangle += 5.0;
+      if (Yangle > 360.0) Yangle -= 360.0;
+      glutPostRedisplay();
+      break;
+    case 'Y':
+      Yangle -= 5.0;
+      if (Yangle < 0.0) Yangle += 360.0;
+      glutPostRedisplay();
+      break;
+    case 'z':
+      Zangle += 5.0;
+      if (Zangle > 360.0) Zangle -= 360.0;
+      glutPostRedisplay();
+      break;
+    case 'Z':
+      Zangle -= 5.0;
+      if (Zangle < 0.0) Zangle += 360.0;
+      glutPostRedisplay();
+      break;
+    default:
+      break;
+    }
 }
 
 // Routine to output interaction instructions to the C++ window.
 void printInteraction(void)
 {
-   cout << "Interaction:" << endl;
-   cout << "Press P/p to increase/decrease the number of longitudinal slices." << endl
-        << "Press Q/q to increase/decrease the number of latitudinal slices." << endl
-        << "Press x, X, y, Y, z, Z to turn the hemisphere." << endl;
+  cout << "Interaction:" << endl;
+  cout << "Press P/p to increase/decrease the number of longitudinal slices." << endl
+       << "Press Q/q to increase/decrease the number of latitudinal slices." << endl
+       << "Press x, X, y, Y, z, Z to turn the hemisphere." << endl;
 }
 
 // Main routine.
 int main(int argc, char **argv) 
 {
-   printInteraction();
-   glutInit(&argc, argv);
+  printInteraction();
+  glutInit(&argc, argv);
 
-   glutInitContextVersion(4, 3);
-   glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+  glutInitContextVersion(4, 3);
+  glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA); 
-   glutInitWindowSize(500, 500);
-   glutInitWindowPosition(100, 100); 
-   glutCreateWindow("hemisphere.cpp");
-   glutDisplayFunc(drawScene); 
-   glutReshapeFunc(resize);  
-   glutKeyboardFunc(keyInput);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA); 
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(100, 100); 
+  glutCreateWindow("hemisphere.cpp");
+  glutDisplayFunc(drawScene); 
+  glutReshapeFunc(resize);  
+  glutKeyboardFunc(keyInput);
 
-   glewExperimental = GL_TRUE;
-   glewInit();
+  glewExperimental = GL_TRUE;
+  glewInit();
 
-   setup(); 
+  setup(); 
    
-   glutMainLoop(); 
+  glutMainLoop(); 
 }
 

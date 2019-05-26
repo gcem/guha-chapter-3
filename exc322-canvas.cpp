bbbefcda4a1cd0cad7a2449c4bc9298d5a259334
gcem <34423323+gcem@users.noreply.github.com>
2019-05-26 09:51:20 +0300

    Exercise 3.22 a

--- exc322-canvas.cpp
+++ exc322-canvas.cpp
@@ -1,15 +1,14 @@
-////////////////////////////////////////////////////////////////////////////////////        
-// canvas.cpp
 //
 // This program allows the user to draw simple shapes on a canvas.
 //
 // Interaction:
 // Left click on a box on the left to select a primitive.
-// Then left click on the drawing area: once for point, twice for line or rectangle.
+// Then left click on the drawing area: once for point, twice for line or rectangle,
+// multiple times for polyline
+// Middle click to end/cancel current drawing
 // Right click for menu options.
 //
-//  Sumanta Guha.
-//////////////////////////////////////////////////////////////////////////////////// 
+
 
 #include <cstdlib>
 #include <vector>
@@ -28,11 +27,12 @@
 
 using namespace std;
 
-#define INACTIVE 0
-#define POINT 1
-#define LINE 2
+#define INACTIVE -1
+#define POINT 0
+#define LINE 1
+#define POLYLINE 2
 #define RECTANGLE 3
-#define NUMBERPRIMITIVES 3
+#define NUMBERPRIMITIVES 4
 
 // Use the STL extension of C++.
 using namespace std;
@@ -49,14 +49,14 @@ static int isGrid = 1; // Is there grid?
 class Point
 {
 public:
-   Point(int xVal, int yVal)
-   {
-	  x = xVal; y = yVal;
-   }
-   void drawPoint(void); // Function to draw a point.
+  Point(int xVal, int yVal)
+  {
+    x = xVal; y = yVal;
+  }
+  void drawPoint(void); // Function to draw a point.
 private:
-   int x, y; // x and y co-ordinates of point.
-   static float size; // Size of point.
+  int x, y; // x and y co-ordinates of point.
+  static float size; // Size of point.
 };
 
 float Point::size = pointSize; // Set point size.
@@ -64,10 +64,10 @@ float Point::size = pointSize; // Set point size.
 // Function to draw a point.
 void Point::drawPoint()
 {  
-   glPointSize(size);
-   glBegin(GL_POINTS);
-      glVertex3f(x, y, 0.0);
-   glEnd();   
+  glPointSize(size);
+  glBegin(GL_POINTS);
+  glVertex3f(x, y, 0.0);
+  glEnd();   
 }
 
 // Vector of points.
@@ -79,36 +79,36 @@ vector<Point>::iterator pointsIterator;
 // Function to draw all points in the points array.
 void drawPoints(void)
 {
-   // Loop through the points array drawing each point.
-   pointsIterator = points.begin();
-   while(pointsIterator != points.end() )
-   {
+  // Loop through the points array drawing each point.
+  pointsIterator = points.begin();
+  while(pointsIterator != points.end() )
+    {
       pointsIterator->drawPoint();
-	  pointsIterator++;
-   }
+      pointsIterator++;
+    }
 }
 
 // Line class.
 class Line
 {
 public:
-   Line(int x1Val, int y1Val, int x2Val, int y2Val)
-   {
-	  x1 = x1Val; y1 = y1Val; x2 = x2Val; y2 = y2Val;
-   }
-   void drawLine();
+  Line(int x1Val, int y1Val, int x2Val, int y2Val)
+  {
+    x1 = x1Val; y1 = y1Val; x2 = x2Val; y2 = y2Val;
+  }
+  void drawLine();
 private:
-   int x1, y1, x2, y2; // x and y co-ordinates of endpoints.
+  int x1, y1, x2, y2; // x and y co-ordinates of endpoints.
 };
 
 
 // Function to draw a line.
 void Line::drawLine()
 {
-   glBegin(GL_LINES);
-      glVertex3f(x1, y1, 0.0);
-      glVertex3f(x2, y2, 0.0);
-   glEnd();
+  glBegin(GL_LINES);
+  glVertex3f(x1, y1, 0.0);
+  glVertex3f(x2, y2, 0.0);
+  glEnd();
 }
 
 // Vector of lines.
@@ -120,33 +120,33 @@ vector<Line>::iterator linesIterator;
 // Function to draw all lines in the lines array.
 void drawLines(void)
 {
-   // Loop through the lines array drawing each line.
-   linesIterator = lines.begin();
-   while(linesIterator != lines.end() )
-   {
+  // Loop through the lines array drawing each line.
+  linesIterator = lines.begin();
+  while(linesIterator != lines.end() )
+    {
       linesIterator->drawLine();
-	  linesIterator++;
-   }
+      linesIterator++;
+    }
 }
 
 // Rectangle class.
 class Rect
 {
 public:
-   Rect(int x1Val, int y1Val, int x2Val, int y2Val)
-   {
-	  x1 = x1Val; y1 = y1Val; x2 = x2Val; y2 = y2Val;
-   }
-   void drawRectangle();
+  Rect(int x1Val, int y1Val, int x2Val, int y2Val)
+  {
+    x1 = x1Val; y1 = y1Val; x2 = x2Val; y2 = y2Val;
+  }
+  void drawRectangle();
 private:
-   int x1, y1, x2, y2; // x and y co-ordinates of diagonally opposite vertices.
+  int x1, y1, x2, y2; // x and y co-ordinates of diagonally opposite vertices.
 };
 
 // Function to draw a rectangle.
 void Rect::drawRectangle()
 {
-   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-   glRectf(x1, y1, x2, y2);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glRectf(x1, y1, x2, y2);
 }
 
 // Vector of rectangles.
@@ -158,319 +158,340 @@ vector<Rect>::iterator rectanglesIterator;
 // Function to draw all rectangles in the rectangles array.
 void drawRectangles(void)
 {
-   // Loop through the rectangles array drawing each rectangle.
-   rectanglesIterator = rectangles.begin();
-   while(rectanglesIterator != rectangles.end() )
-   {
+  // Loop through the rectangles array drawing each rectangle.
+  rectanglesIterator = rectangles.begin();
+  while(rectanglesIterator != rectangles.end() )
+    {
       rectanglesIterator->drawRectangle();
-	  rectanglesIterator++;
-   }
+      rectanglesIterator++;
+    }
 }
 
+// draw empty selection boxes to draw icons in
+void drawSelectionBoxes()
+{
+  for (int i = 0; i < NUMBERPRIMITIVES; i++)
+    {
+      if (primitive == i) glColor3f(1.0, 1.0, 1.0); // Highlight.
+      else glColor3f(0.8, 0.8, 0.8); // No highlight.
+      glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+      glRectf(0.0, (1 - i / 10.0) * height, 0.1*width, -height);
+      
+      // Draw black boundary.
+      glColor3f(0.0, 0.0, 0.0); 
+      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+      glRectf(0.0, (1 - i / 10.0) * height, 0.1*width, -height); 
+
+    }
+}
+
+
 // Function to draw point selection box in left selection area.
-void drawPointSelectionBox(void)
+void drawPointSelectionIcon(void)
 {  
-   if (primitive == POINT) glColor3f(1.0, 1.0, 1.0); // Highlight.
-   else glColor3f(0.8, 0.8, 0.8); // No highlight.
-   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-   glRectf(0.0, 0.9*height, 0.1*width, height);
-
-   // Draw black boundary.
-   glColor3f(0.0, 0.0, 0.0); 
-   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-   glRectf(0.0, 0.9*height, 0.1*width, height); 
-
-   // Draw point icon.
-   glPointSize(pointSize);
-   glColor3f(0.0, 0.0, 0.0);
-   glBegin(GL_POINTS);
-      glVertex3f(0.05*width, 0.95*height, 0.0);
-   glEnd();  
+  // Draw point icon.
+  glPointSize(pointSize);
+  glColor3f(0.0, 0.0, 0.0);
+  glBegin(GL_POINTS);
+  glVertex3f(0.05*width, 0.95*height - height * POINT / 10.0, 0.0);
+  glEnd();  
 }
 
 // Function to draw line selection box in left selection area.
-void drawLineSelectionBox(void)
+void drawLineSelectionIcon(void)
 {  
-   if (primitive == LINE) glColor3f(1.0, 1.0, 1.0); // Highlight.
-   else glColor3f(0.8, 0.8, 0.8); // No highlight.
-   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-   glRectf(0.0, 0.8*height, 0.1*width, 0.9*height);
+  // Draw line icon.
+  glColor3f(0.0, 0.0, 0.0);
+  glBegin(GL_LINES);
+  glVertex3f(0.025*width, 0.925*height - height * LINE / 10.0, 0.0);
+  glVertex3f(0.075*width, 0.975*height - height * LINE / 10.0, 0.0);
+  glEnd();  
+}
 
-   // Draw black boundary.
-   glColor3f(0.0, 0.0, 0.0);
-   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-   glRectf(0.0, 0.8*height, 0.1*width, 0.9*height);
-
-   // Draw line icon.
-   glColor3f(0.0, 0.0, 0.0);
-   glBegin(GL_LINES);
-      glVertex3f(0.025*width, 0.825*height, 0.0);
-      glVertex3f(0.075*width, 0.875*height, 0.0);
-   glEnd();  
+void drawPolyLineSelectionIcon()
+{
+  glColor3f(0.0, 0.0, 0.0);
+  glBegin(GL_LINE_STRIP);
+  glVertex3f(0.02*width, 0.925*height - height * POLYLINE / 10.0, 0.0);
+  glVertex3f(0.045*width, 0.975*height - height * POLYLINE / 10.0, 0.0);
+  glVertex3f(0.055*width, 0.925*height - height * POLYLINE / 10.0, 0.0);
+  glVertex3f(0.08*width, 0.975*height - height * POLYLINE / 10.0, 0.0);
+  glEnd();
 }
 
 // Function to draw rectangle selection box in left selection area.
-void drawRectangleSelectionBox(void)
+void drawRectangleSelectionIcon(void)
 {  
-   if (primitive == RECTANGLE) glColor3f(1.0, 1.0, 1.0); // Highlight.
-   else glColor3f(0.8, 0.8, 0.8); // No highlight.
-   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-   glRectf(0.0, 0.7*height, 0.1*width, 0.8*height);
-
-   // Draw black boundary.
-   glColor3f(0.0, 0.0, 0.0);
-   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-   glRectf(0.0, 0.7*height, 0.1*width, 0.8*height);
-
-   // Draw rectangle icon.
-   glColor3f(0.0, 0.0, 0.0);
-   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-   glRectf(0.025*width, 0.735*height, 0.075*width, 0.765*height);
-   glEnd();  
+  // Draw rectangle icon.
+  glColor3f(0.0, 0.0, 0.0);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glRectf(0.025*width,
+	  0.935*height - height * RECTANGLE / 10.0, 0.075*width,
+	  0.965*height - height * RECTANGLE / 10.0);
+  glEnd();  
 }
 
 // Function to draw unused part of left selection area.
 void drawInactiveArea(void)
 {
-   glColor3f(0.6, 0.6, 0.6);
-   glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-   glRectf(0.0, 0.0, 0.1*width, (1 - NUMBERPRIMITIVES*0.1)*height);
+  glColor3f(0.6, 0.6, 0.6);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  glRectf(0.0, 0.0, 0.1*width, (1 - NUMBERPRIMITIVES*0.1)*height);
 
-   glColor3f(0.0, 0.0, 0.0);
-   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-   glRectf(0.0, 0.0, 0.1*width, (1 - NUMBERPRIMITIVES*0.1)*height);
+  glColor3f(0.0, 0.0, 0.0);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glRectf(0.0, 0.0, 0.1*width, (1 - NUMBERPRIMITIVES*0.1)*height);
 }
 
 // Function to draw temporary point.
 void drawTempPoint(void)
 {
-   glColor3f(1.0, 0.0, 0.0);
-   glPointSize(pointSize);
-   glBegin(GL_POINTS);
-      glVertex3f(tempX, tempY, 0.0);
-   glEnd(); 
+  glColor3f(1.0, 0.0, 0.0);
+  glPointSize(pointSize);
+  glBegin(GL_POINTS);
+  glVertex3f(tempX, tempY, 0.0);
+  glEnd(); 
 }
 
 // Function to draw a grid.
 void drawGrid(void)
 {
-   int i;
+  int i;
    
-   glEnable(GL_LINE_STIPPLE);
-   glLineStipple(1, 0x5555); 
-   glColor3f(0.75, 0.75, 0.75);
+  glEnable(GL_LINE_STIPPLE);
+  glLineStipple(1, 0x5555); 
+  glColor3f(0.75, 0.75, 0.75);
 
-   glBegin(GL_LINES);
-	  for (i=2; i <=9; i++)
-	  {
-         glVertex3f(i*0.1*width, 0.0, 0.0);
-         glVertex3f(i*0.1*width, height, 0.0);
-	  }
-	  for (i=1; i <=9; i++)
-	  {
-         glVertex3f(0.1*width, i*0.1*height, 0.0);
-         glVertex3f(width, i*0.1*height, 0.0);
-	  }
-   glEnd();
-   glDisable(GL_LINE_STIPPLE);
+  glBegin(GL_LINES);
+  for (i=2; i <=9; i++)
+    {
+      glVertex3f(i*0.1*width, 0.0, 0.0);
+      glVertex3f(i*0.1*width, height, 0.0);
+    }
+  for (i=1; i <=9; i++)
+    {
+      glVertex3f(0.1*width, i*0.1*height, 0.0);
+      glVertex3f(width, i*0.1*height, 0.0);
+    }
+  glEnd();
+  glDisable(GL_LINE_STIPPLE);
 }
 
 // Drawing routine.
 void drawScene(void)
 {
-   glClear(GL_COLOR_BUFFER_BIT);
-   glColor3f(0.0, 0.0, 0.0); 
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(0.0, 0.0, 0.0); 
 
-   drawPointSelectionBox();
-   drawLineSelectionBox();
-   drawRectangleSelectionBox();
-   drawInactiveArea();
+  drawSelectionBoxes();
+  drawPointSelectionIcon();
+  drawLineSelectionIcon();
+  drawPolyLineSelectionIcon();
+  drawRectangleSelectionIcon();
+  drawInactiveArea();
 
-   drawPoints();
-   drawLines();
-   drawRectangles();
-   if ( ((primitive == LINE) || (primitive == RECTANGLE)) &&
-	   (pointCount == 1) ) drawTempPoint();
-   if (isGrid) drawGrid();
+  drawPoints();
+  drawLines();
+  drawRectangles();
 
-   glutSwapBuffers();
+  if(pointCount == 1) drawTempPoint();
+  if (isGrid) drawGrid();
+
+  glutSwapBuffers();
 }
 
 // Function to pick primitive if click is in left selection area.
 void pickPrimitive(int y)
 {
-   if ( y < (1- NUMBERPRIMITIVES*0.1)*height ) primitive = INACTIVE;
-   else if ( y < (1 - 2*0.1)*height ) primitive = RECTANGLE;
-   else if ( y < (1 - 1*0.1)*height ) primitive = LINE;
-   else primitive = POINT;
+  int primitiveIndex = 10 * (height - y) / height; // round down the result
+  if (primitiveIndex >= NUMBERPRIMITIVES) primitive = INACTIVE;
+  else primitive = primitiveIndex;
 }
 
 // The mouse callback routine.
 void mouseControl(int button, int state, int x, int y)
 {
-   if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-   {
+  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
       y = height - y; // Correct from mouse to OpenGL co-ordinates.
 
-	  // Click outside canvas - do nothing.
+      
+      // Click outside canvas - do nothing.
       if ( x < 0 || x > width || y < 0 || y > height ) ;
 
-	  // Click in left selection area.
+      // Click in left selection area.
       else if ( x < 0.1*width ) 
-	  {
-	     pickPrimitive(y);
-		 pointCount = 0;
-	  }
+	{
+	  pickPrimitive(y);
+	  pointCount = 0;
+	}
 
-	  // Click in canvas.
+      // Click in canvas.
       else
-	  {
-	     if (primitive == POINT) points.push_back( Point(x,y) );
-         else if (primitive == LINE) 
-		 { 
-	        if (pointCount == 0)
-			{
-               tempX = x; tempY = y;
-		       pointCount++;
-			}
-	        else 
-			{
-               lines.push_back( Line(tempX, tempY, x, y) );
-		       pointCount = 0;
-			}
-		 }
-         else if (primitive == RECTANGLE) 
-		 { 
-	        if (pointCount == 0)
-			{
-               tempX = x; tempY = y;
-		       pointCount++;
-			}
-	        else 
-			{
-               rectangles.push_back( Rect(tempX, tempY, x, y) );
-		       pointCount = 0;
-			}
-		 }
-	  }
-   }
-   glutPostRedisplay();
+	{
+	  if (primitive == POINT) points.push_back( Point(x,y) );
+	  else if (primitive == LINE) 
+	    { 
+	      if (pointCount == 0)
+		{
+		  tempX = x; tempY = y;
+		  pointCount++;
+		}
+	      else 
+		{
+		  lines.push_back( Line(tempX, tempY, x, y) );
+		  pointCount = 0;
+		}	      
+	    }
+	  else if (primitive == POLYLINE) 
+	    { 
+	      if (pointCount == 0)
+		{
+		  tempX = x; tempY = y;
+		  pointCount++;
+		}
+	      else 
+		{
+		  lines.push_back( Line(tempX, tempY, x, y) );
+		  tempX = x; tempY = y;
+		}
+	    }
+	  else if (primitive == RECTANGLE) 
+	    { 
+	      if (pointCount == 0)
+		{
+		  tempX = x; tempY = y;
+		  pointCount++;
+		}
+	      else 
+		{
+		  rectangles.push_back( Rect(tempX, tempY, x, y) );
+		  pointCount = 0;
+		}
+	    }
+	}
+    }
+  else if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN)
+    {
+      pointCount = 0;
+    }
+  glutPostRedisplay();
 }
 
 // OpenGL window reshape routine.
 void resize(int w, int h)
 {
-   glViewport(0, 0, w, h);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
+  glViewport(0, 0, w, h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
 
-   // Set viewing box dimensions equal to window dimensions.
-   glOrtho(0.0, (float)w, 0.0, (float)h, -1.0, 1.0);
+  // Set viewing box dimensions equal to window dimensions.
+  glOrtho(0.0, (float)w, 0.0, (float)h, -1.0, 1.0);
    
-   // Pass the size of the OpenGL window to globals.
-   width = w; 
-   height = h;  
+  // Pass the size of the OpenGL window to globals.
+  width = w; 
+  height = h;  
 
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
 }
 
 // Keyboard input processing routine.
 void keyInput(unsigned char key, int x, int y)
 {
-   switch (key) 
-   {
-      case 27:
-         exit(0);
-         break;
-      default:
-         break;
-   }
+  switch (key) 
+    {
+    case 27:
+      exit(0);
+      break;
+    default:
+      break;
+    }
 }
 
 // Clear the canvas and reset for fresh drawing.
 void clearAll(void)
 {
-   points.clear();
-   lines.clear();
-   rectangles.clear();
-   primitive = INACTIVE;
-   pointCount = 0;
+  points.clear();
+  lines.clear();
+  rectangles.clear();
+  primitive = INACTIVE;
+  pointCount = 0;
 }
 
 // The right button menu callback function.
 void rightMenu(int id)
 {
-   if (id==1) 
-   {
-	  clearAll();
-	  glutPostRedisplay();
-   }
-   if (id==2) exit(0);
+  if (id==1) 
+    {
+      clearAll();
+      glutPostRedisplay();
+    }
+  if (id==2) exit(0);
 }
 
 // The sub-menu callback function.
 void grid_menu(int id)
 {
-   if (id==3) isGrid = 1;
-   if (id==4) isGrid = 0;
-   glutPostRedisplay();
+  if (id==3) isGrid = 1;
+  if (id==4) isGrid = 0;
+  glutPostRedisplay();
 }
 
 // Function to create menu.
 void makeMenu(void)
 {
-   int sub_menu;
-   sub_menu = glutCreateMenu(grid_menu);
-   glutAddMenuEntry("On", 3);
-   glutAddMenuEntry("Off",4);
+  int sub_menu;
+  sub_menu = glutCreateMenu(grid_menu);
+  glutAddMenuEntry("On", 3);
+  glutAddMenuEntry("Off",4);
 
-   glutCreateMenu(rightMenu);
-   glutAddSubMenu("Grid", sub_menu);
-   glutAddMenuEntry("Clear",1);
-   glutAddMenuEntry("Quit",2);
-   glutAttachMenu(GLUT_RIGHT_BUTTON);
+  glutCreateMenu(rightMenu);
+  glutAddSubMenu("Grid", sub_menu);
+  glutAddMenuEntry("Clear",1);
+  glutAddMenuEntry("Quit",2);
+  glutAttachMenu(GLUT_RIGHT_BUTTON);
 }
 
 // Initialization routine.
 void setup(void) 
 {
-   glClearColor(1.0, 1.0, 1.0, 0.0); 
+  glClearColor(1.0, 1.0, 1.0, 0.0); 
 
-   makeMenu(); // Create menu.
+  makeMenu(); // Create menu.
 }
 
 // Routine to output interaction instructions to the C++ window.
 void printInteraction(void)
 {
-   cout << "Interaction:" << endl;
-   cout << "Left click on a box on the left to select a primitive." << endl
-        << "Then left click on the drawing area: once for point, twice for line or rectangle." << endl
-        << "Right click for menu options." <<  endl; 
+  cout << "Interaction:" << endl;
+  cout << "Left click on a box on the left to select a primitive." << endl
+       << "Then left click on the drawing area: once for point, twice for line or rectangle." << endl
+       << "Right click for menu options." <<  endl; 
 }
 
 // Main routine.
 int main(int argc, char **argv) 
 {
-   printInteraction();
-   glutInit(&argc, argv);
+  printInteraction();
+  glutInit(&argc, argv);
 
-   glutInitContextVersion(4, 3);
-   glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+  glutInitContextVersion(4, 3);
+  glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA); 
-   glutInitWindowSize(500, 500);
-   glutInitWindowPosition(100, 100); 
-   glutCreateWindow("canvas.cpp");
-   glutDisplayFunc(drawScene); 
-   glutReshapeFunc(resize);  
-   glutKeyboardFunc(keyInput);
-   glutMouseFunc(mouseControl); 
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA); 
+  glutInitWindowSize(500, 500);
+  glutInitWindowPosition(100, 100); 
+  glutCreateWindow("canvas.cpp");
+  glutDisplayFunc(drawScene); 
+  glutReshapeFunc(resize);  
+  glutKeyboardFunc(keyInput);
+  glutMouseFunc(mouseControl); 
 
-   glewExperimental = GL_TRUE;
-   glewInit();
+  glewExperimental = GL_TRUE;
+  glewInit();
 
-   setup(); 
+  setup(); 
 
-   glutMainLoop(); 
+  glutMainLoop(); 
 }
